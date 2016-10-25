be9f40d362d2491e1fcf3b12ac3f0672234070e6
hmalatini <nani93@gmail.com>
2016-10-25 15:19:49 -0300

    primer commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/turtlebot/Programming_Robots_with_ROS/HermesIII/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/turtlebot/Programming_Robots_with_ROS/HermesIII/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/turtlebot/Programming_Robots_with_ROS/HermesIII/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/turtlebot/Programming_Robots_with_ROS/HermesIII/src:$ROS_PACKAGE_PATH"
---
+++ build/hermesIII/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/turtlebot/Programming_Robots_with_ROS/HermesIII/src/hermesIII
+# Build directory: /home/turtlebot/Programming_Robots_with_ROS/HermesIII/build/hermesIII
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
