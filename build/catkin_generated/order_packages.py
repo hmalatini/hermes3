0286b3ed046cc3315560383e7d16c1e5b72b47cb
Hernán <nani93@gmail.com>
2016-10-27 15:54:35 -0300

    Se creo el gitignore, y se preparo el repo para que no dependa de los directorios

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/turtlebot/Programming_Robots_with_ROS/HermesIII/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/indigo".split(';') if "/opt/ros/indigo" != "" else []
