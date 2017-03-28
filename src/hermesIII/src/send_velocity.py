65decd2f9d0bce91313833e1fe354b273486ce62
Hernán <nani93@gmail.com>
2017-03-28 20:37:30 -0300

    PID Se agrega launch de PID, y ademas el nodo que va a pedir las velocidades a Arduino

---
+++ src/hermesIII/src/send_velocity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import UInt8
+
+def talker():
+    pub = rospy.Publisher('send_velocity', UInt8, queue_size=10)
+    rospy.init_node('velocity_request', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        pub.publish(1)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
